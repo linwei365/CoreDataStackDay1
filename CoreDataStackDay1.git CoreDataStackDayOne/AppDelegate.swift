b6d8a4181375b40f2fc1d5f8376a575c8828cf8f
linwei365 <linwei365@gmail.com>
2016-02-12 12:23:04 -0800

    added coreDataStack added model and created instance in app delegate

--- CoreDataStackDay1.git CoreDataStackDayOne/AppDelegate.swift
+++ CoreDataStackDay1.git CoreDataStackDayOne/AppDelegate.swift
@@ -13,10 +13,21 @@ import CoreData
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-
+    //9
+    lazy var coreDataStack = CoreDataStack()
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
+        
+        //10
+        let root =  self.window?.rootViewController as! UINavigationController
+        let viewController = root.topViewController as! TableViewController
+        
+        viewController.managedObjectContext = coreDataStack.mangedObjectContext
+        
+        
+        
+        
         return true
     }
 
@@ -28,6 +39,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationDidEnterBackground(application: UIApplication) {
         // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
         // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+        
+        coreDataStack.save()
     }
 
     func applicationWillEnterForeground(application: UIApplication) {
@@ -42,6 +55,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
         // Saves changes in the application's managed object context before the application terminates.
         self.saveContext()
+        
+        coreDataStack.save()
     }
 
     // MARK: - Core Data stack
---
+++ Student.swift
@@ -0,0 +1,17 @@
+//
+//  Student.swift
+//  CoreDataStackDay1.git CoreDataStackDayOne
+//
+//  Created by Lin Wei on 2/12/16.
+//  Copyright © 2016 Lin Wei. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+
+class Student: NSManagedObject {
+
+// Insert code here to add functionality to your managed object subclass
+
+}
