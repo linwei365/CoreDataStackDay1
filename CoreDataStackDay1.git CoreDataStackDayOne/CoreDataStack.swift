7ee596e0f323408f19d5d87f07c93fad26f0d668
linwei365 <linwei365@gmail.com>
2016-02-12 08:48:58 -0800

    new branch

---
+++ CoreDataStackDay1.git CoreDataStackDayOne/CoreDataStack.swift
@@ -0,0 +1,9 @@
+//
+//  CoreDataStack.swift
+//  CoreDataStackDay1.git CoreDataStackDayOne
+//
+//  Created by Lin Wei on 2/12/16.
+//  Copyright © 2016 Lin Wei. All rights reserved.
+//
+
+import Foundation
