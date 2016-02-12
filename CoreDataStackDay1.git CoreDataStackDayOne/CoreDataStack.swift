9603f264d5bbec4d54acdaee854c3e43d679706d
linwei365 <linwei365@gmail.com>
2016-02-12 09:18:30 -0800

    initialized mangedOjbectModel persistentStoreCoordinate mangedObjectContext applicationDoucmentDirectory

--- CoreDataStackDay1.git CoreDataStackDayOne/CoreDataStack.swift
+++ CoreDataStackDay1.git CoreDataStackDayOne/CoreDataStack.swift
@@ -7,3 +7,69 @@
 //
 
 import Foundation
+// 1
+import CoreData
+
+//2
+class CoreDataStack {
+    
+    //3 
+    let mangedObjectContext: NSManagedObjectContext?
+    
+    let persistentStoreCooordinator: NSPersistentStoreCoordinator?
+    
+    let managedObjectModel: NSManagedObjectModel?
+    
+    init(){
+        
+        let bundle = NSBundle.mainBundle()
+        let modelURL =  bundle.URLForResource("Data", withExtension: "momd")!
+        
+        managedObjectModel = NSManagedObjectModel(contentsOfURL: modelURL)
+        
+        persistentStoreCooordinator = NSPersistentStoreCoordinator(managedObjectModel: managedObjectModel!)
+        
+        mangedObjectContext = NSManagedObjectContext()
+        mangedObjectContext?.persistentStoreCoordinator = persistentStoreCooordinator
+        
+        
+        //5
+        let appDirectory = applicationDocumentDirectory()
+        let storeURL = appDirectory.URLByAppendingPathComponent("Data")
+        let migrateOption = [NSMigratePersistentStoresAutomaticallyOption: true]
+        
+        var err: NSError?
+        do {
+            try persistentStoreCooordinator?.addPersistentStoreWithType(NSSQLiteStoreType, configuration: nil, URL: storeURL, options: migrateOption)
+            
+        } catch let error1 as NSError {
+            err = error1
+            
+            
+        }
+        if (err != nil){
+            
+            print("failed to create store")
+            
+            abort()
+        }
+        
+    }
+    
+    
+    //4
+    
+    func applicationDocumentDirectory() -> NSURL {
+    
+    let fileManager = NSFileManager.defaultManager()
+        let urls = fileManager.URLsForDirectory(.DocumentationDirectory, inDomains: .UserDomainMask)
+        
+        return urls[0];
+    
+    }
+    
+    
+    
+    
+    
+}
